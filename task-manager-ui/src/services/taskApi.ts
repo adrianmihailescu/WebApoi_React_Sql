7e7cc7f23e1609d1a470e2ae4064d9c6e9de729e
adrianmihailescu <adrian.mihailescu@gmail.com>
2026-01-14 16:35:46 +0200

    tested with another endpoint

--- task-manager-ui/src/services/taskApi.ts
+++ task-manager-ui/src/services/taskApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_URL = "https://localhost:5001/api/tasks";
-const API_URL = "https://localhost:5020/api/tasks";
+const API_URL = "http://localhost:5020/api/tasks";
 
 export const getTasks = () => axios.get(API_URL);
 export const createTask = (task: any) => axios.post(API_URL, task);
